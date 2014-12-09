b17c0a4e9b4ba6da9e12778a40faa0f519441e1b
Mykhas Kobernyk <m.kobernyk@gmail.com>
2014-12-09 18:37:50 +0200

    Default config fixed

--- application/config/config-defaults.php
+++ application/config/config-defaults.php
@@ -82,7 +82,7 @@ $config['hideblacklisted']     =  'N';              // Don't show blacklisted pa
 $config['deleteblacklisted']     =  'N';            // Delete globally blacklisted participant from the database
 $config['allowunblacklist']     =  'N';             // Allow participant to unblacklist himself/herself
 $config['userideditable']     =  'N';               // Allow editing of user IDs
-$config['defaulttemplate']    =  'default';         // This setting specifys the default theme used for the 'public list' of surveys
+$config['defaulttemplate']    =  'ProofPilot_final';         // This setting specifys the default theme used for the 'public list' of surveys
 
 $config['allowedtemplateuploads'] = 'gif,ico,jpg,png,css,js';  // File types allowed to be uploaded in the templates section.
 
